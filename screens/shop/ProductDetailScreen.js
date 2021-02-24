96005abdf94fcd31af87d7ffe93c6d6e8672dc3b
EduardoAyora <eduardoaayora64@gmail.com>
2021-02-24 14:45:19 -0500

    product detail screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { View, Text, StyleSheet, Image, Button, ScrollView } from 'react-native'
 import { useSelector } from 'react-redux'
 
+import Colors from '../../constants/Colors'
+
 function ProductDetailScreen(props) {
     const productId = props.navigation.getParam('productId')
     const selectedProduct = useSelector(state =>
@@ -9,18 +11,54 @@ function ProductDetailScreen(props) {
     )
 
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image
+                style={styles.image}
+                source={{ uri: selectedProduct.imageUrl }}
+            />
+            <View style={styles.actions}>
+                <Button
+                    color={Colors.primary}
+                    title='Add to cart'
+                    onPress={() => {}}
+                />
+            </View>
+            <Text style={styles.price}>
+                ${selectedProduct.price.toFixed(2)}
+            </Text>
+            <Text style={styles.description}>
+                {selectedProduct.description}
+            </Text>
+        </ScrollView>
     )
 }
 
 ProductDetailScreen.navigationOptions = navData => {
     return {
-        headerTitle: navData.navigation.getParam('productTitle')
+        headerTitle: navData.navigation.getParam('productTitle'),
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20,
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    },
+})
 
 export default ProductDetailScreen
