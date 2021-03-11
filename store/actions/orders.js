2f93eb02e01af85fde657aa600a71e501db6b8ed
EduardoAyora <eduardoaayora64@gmail.com>
2021-03-11 14:04:52 -0500

    guardando ordenes

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -3,8 +3,38 @@ import CartItem from '../../components/shop/CartItem'
 export const ADD_ORDER = 'ADD_ORDER'
 
 export const addOrder = (cartItems, totalAmount) => {
-    return {
-        type: ADD_ORDER,
-        orderData: { items: cartItems, amount: totalAmount },
+    return async dispatch => {
+        const date = new Date()
+
+        const response = await fetch(
+            'https://rapiweb-105b6-default-rtdb.firebaseio.com/orders/u1.json',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    cartItems,
+                    totalAmount,
+                    date: date.toISOString(),
+                }),
+            }
+        )
+
+        if (!response.ok) {
+            throw new Error('Algo salió mal')
+        }
+
+        const resData = await response.json()
+
+        dispatch({
+            type: ADD_ORDER,
+            orderData: {
+                id: resData.name,
+                items: cartItems,
+                amount: totalAmount,
+                date: date,
+            },
+        })
     }
 }
